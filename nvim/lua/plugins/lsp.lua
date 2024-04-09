80ea24f091115d5920d273529cbf1f4fd3cf948a
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-04-10 00:54:29 +0100

    feat(nvim): add autocomplete

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,12 +11,20 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lsp = require("lspconfig")
-      lsp.lua_ls.setup({})
-      lsp.tailwindcss.setup({})
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      lsp.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lsp.tailwindcss.setup({
+        capabilities = capabilities,
+      })
       lsp.tsserver.setup({
+        capabilities = capabilities,
         filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
         cmd = { "typescript-language-server", "--stdio" },
       })
+
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})

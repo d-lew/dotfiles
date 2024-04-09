14add62784f29fc5a5344b632fcb628bf6e585b6
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-04-10 00:55:46 +0100

    feat(nvim): add transparent background

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,6 +4,11 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      transparent_background = true,
+      show_end_of_buffer = true,
+    })
+
     vim.cmd.colorscheme("catppuccin")
   end,
 }

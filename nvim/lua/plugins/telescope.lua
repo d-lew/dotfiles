e78fbb44dd4f61fa45e754c14420047adc3f4d03
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-04-09 23:26:13 +0100

    feat(nvim): include hidden files in `telescope` pickers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,12 +4,16 @@ return {
     tag = "0.1.6",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
+      defaults = {
+        file_ignore_patterns = { ".git" },
+      },
       pickers = {
         find_files = {
           follow = true,
+          hidden = true,
         },
         live_grep = {
-          additional_args = { "-L" },
+          additional_args = { "-L", "--hidden" },
         },
       },
     },

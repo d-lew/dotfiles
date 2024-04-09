4a3b890daa7df1058a5e92289a357242e2411b5a
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-04-09 23:04:27 +0100

    feat(nvim): add `tailwind` and `tsserver` LSPs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "tsserver", "tailwindcss" },
       })
     end
   },
@@ -12,9 +12,14 @@ return {
     config = function()
       local lsp = require("lspconfig")
       lsp.lua_ls.setup({})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      lsp.tailwindcss.setup({})
+      lsp.tsserver.setup({
+        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+        cmd = { "typescript-language-server", "--stdio" },
+      })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }

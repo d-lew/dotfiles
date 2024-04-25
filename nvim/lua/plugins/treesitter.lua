beabac136d6d913646f19382d4781ff2c68062e9
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-04-25 21:44:57 +0100

    feat(nvim): add `treesitter-text-objects`

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -10,10 +10,28 @@ return {
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+          },
+        },
       })
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
 }

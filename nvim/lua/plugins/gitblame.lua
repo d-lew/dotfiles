ff9068fb52e06cf38b558e5109cfcdfa0e13fd70
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-05-02 00:45:19 +0100

    feat(nvim): add git blame

---
+++ nvim/lua/plugins/gitblame.lua
@@ -0,0 +1,15 @@
+return {
+  "f-person/git-blame.nvim",
+  lazy = true,
+  keys = {
+    { "<leader>gb", ":GitBlameToggle<CR>" },
+  },
+  config = function()
+    require("gitblame").setup({
+      enabled = false,
+      date_format = "%r",
+      message_template = "  <author>, <date> • <summary>",
+      message_when_not_committed = "",
+    })
+  end,
+}

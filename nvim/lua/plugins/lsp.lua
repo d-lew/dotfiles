1dc30b3e82ef1ffe1e7cd44ec67e9d52cf313f0d
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-05-02 01:50:13 +0100

    fix(nvim): automatically configure LSPs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,8 +4,20 @@ return {
     lazy = true,
     event = "BufNew",
     config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "tailwindcss" },
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          require("lspconfig")[server_name].setup(capabilities)
+        end,
+        ["tsserver"] = function()
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          require("lspconfig")["tsserver"].setup({
+            capabilities = capabilities,
+            filetypes = { "typescript", "javascript", "typescriptreact", "typescript.tsx" },
+            cmd = { "typescript-language-server", "--stdio" },
+          })
+        end,
       })
     end,
   },
@@ -14,21 +26,6 @@ return {
     lazy = true,
     event = "BufNew",
     config = function()
-      local lsp = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      lsp.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lsp.tailwindcss.setup({
-        capabilities = capabilities,
-      })
-      lsp.tsserver.setup({
-        capabilities = capabilities,
-        filetypes = { "typescript", "javascript", "typescriptreact", "typescript.tsx" },
-        cmd = { "typescript-language-server", "--stdio" },
-      })
-
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", function()

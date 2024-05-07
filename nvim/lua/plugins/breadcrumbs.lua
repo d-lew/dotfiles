b0e49b995552366148e0c4c62d09d75afdc55dc6
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-05-07 23:29:55 +0100

    fix(nvim): load `barbeque` non-lazy
    when using nvim for git it would get in the way

--- nvim/lua/plugins/breadcrumbs.lua
+++ nvim/lua/plugins/breadcrumbs.lua
@@ -2,8 +2,6 @@ return {
   "utilyre/barbecue.nvim",
   name = "barbecue",
   version = "*",
-  lazy = true,
-  event = "VeryLazy",
   dependencies = {
     "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons",

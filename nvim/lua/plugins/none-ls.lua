ccdb646fefc62ddb5dcb23ef0bcfdae7753073b8
Dan Lewis <92388017+d-lew@users.noreply.github.com>
2024-05-08 21:45:57 +0100

    feat(nvim): spelling diagnostics using `codespell`

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -8,6 +8,7 @@ return {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.prettier,
           require("none-ls.diagnostics.eslint_d"),
+          null_ls.builtins.diagnostics.codespell,
         },
       })
     end,
